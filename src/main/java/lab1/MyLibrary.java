082aa1f687e08e8317d9131416e3bebe6ed17e76
Antoine Heurtier <antoine.heurtier@epfl.ch>
2026-09-07 18:18:56 +0800

    This is an initial commit for Comp3111LEx Project

---
+++ src/main/java/lab1/MyLibrary.java
@@ -0,0 +1,23 @@
+package lab1;
+
+
+/**
+ * @author Antoine Heurtier (393038)
+ */
+public class MyLibrary {
+    public static int Power(int Base, int Exponent){
+        if (Exponent==1){
+            return Base * Exponent;
+        } else{
+            return Base * Power(Base, Exponent - 1);
+        }
+    }
+    public static int factorial(int n){
+        if (n==1){
+            return 1;
+        } else{
+            return n * factorial(n-1);
+        }
+    }
+}
+
